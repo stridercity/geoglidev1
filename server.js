2df5e86bcef8eddb5da1252b9b1d5c28b79ae18b
stridercity <152151609+stridercity@users.noreply.github.com>
2023-12-28 10:35:18 +0200

    updated code4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 const path = require('path');
-const fs = require('fs'); // Include the fs module
+const fs = require('fs');
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -11,18 +11,17 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/save-video', upload.single('video'), (req, res) => {
-    const videoBuffer = req.file.buffer; // Buffer containing the video data
+    const videoBuffer = req.file.buffer;
 
-    // Specify the file path where you want to save the video
     const filePath = path.join(__dirname, 'uploads', 'animation.mp4');
 
-    // Write the buffer to a file
     fs.writeFile(filePath, videoBuffer, (err) => {
         if (err) {
             console.error('Error saving video:', err);
             res.status(500).json({ success: false, message: 'Error saving video' });
         } else {
             console.log('Video saved successfully');
+            res.setHeader('Content-Type', 'application/json'); // Set the content type to JSON
             res.json({ success: true, message: 'Video saved successfully' });
         }
     });
