44d1a1996c49b6cf35cfd54135d530169013218c
stridercity <152151609+stridercity@users.noreply.github.com>
2023-12-28 10:43:45 +0200

    updated code 5

--- server.js
+++ server.js
@@ -1,33 +1,45 @@
 const express = require('express');
-const app = express();
 const multer = require('multer');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set up Multer for file uploads
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-app.use(express.json());
-app.use(express.static('public'));
-
+// Define the /save-video route
 app.post('/save-video', upload.single('video'), (req, res) => {
-    const videoBuffer = req.file.buffer;
+    console.log('Received POST request to /save-video');
 
+    if (!req.file) {
+        console.error('No video file received');
+        return res.status(400).json({ success: false, message: 'No video file received' });
+    }
+
+    const videoBuffer = req.file.buffer; // Buffer containing the video data
+
+    // Specify the file path where you want to save the video
     const filePath = path.join(__dirname, 'uploads', 'animation.mp4');
 
+    // Write the buffer to a file
     fs.writeFile(filePath, videoBuffer, (err) => {
         if (err) {
             console.error('Error saving video:', err);
-            res.status(500).json({ success: false, message: 'Error saving video' });
-        } else {
-            console.log('Video saved successfully');
-            res.setHeader('Content-Type', 'application/json'); // Set the content type to JSON
-            res.json({ success: true, message: 'Video saved successfully' });
+            return res.status(500).json({ success: false, message: 'Error saving video' });
         }
+
+        console.log('Video saved successfully');
+        res.json({ success: true, message: 'Video saved successfully' });
     });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Serve static files from the 'uploads' directory
+app.use('/uploads', express.static('uploads'));
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
